9b5b66c7a3f346f9cbfa6f38913e94e31e19e8fc
Sander de Groot <sander@degroot.dev>
2026-02-13 13:43:01 +0100

    chore: upgrade to 1.0.2 of the build plugins

--- flux-basic-java/build.gradle.kts
+++ flux-basic-java/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("io.fluxzero.tools.gradle.plugin") version "1.0.1"
+    id("io.fluxzero.tools.gradle.plugin") version "1.0.2"
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
 }
